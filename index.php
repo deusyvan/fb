ea1ddb1522da9ed62f84ae453d47d055b7a80e7a
Deusyvan Silva <deusyvan@dfsweb.com.br>
2019-12-09 22:41:38 -0300

    Mais sobre a api

--- index.php
+++ index.php
@@ -3,11 +3,14 @@ require 'fb.php';
 
 if(isset($_SESSION['fb_access_token']) && !empty($_SESSION['fb_access_token'])){
     //echo "Tá logado";
-    $res = $fb->get('/me?fields=email,name,id', $_SESSION['fb_access_token']);
+    $res = $fb->get('/me?fields=email,name,id,picture', $_SESSION['fb_access_token']);
     
     $r = json_decode($res->getBody());
     
     //print_r($r);
+    $img = $r->picture->data->url;
+    
+    echo "<img src='".$img."' border='0' /><br/>";
     
     echo "Meu nome: ".$r->name;
     
